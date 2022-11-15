2825277f8b57e1a7246a214bc2873931712b431a
Jppeaser <71985884+Jppeaser@users.noreply.github.com>
2022-11-15 10:26:10 +0100

    Add files via upload

---
+++ src/Components/ScoreList.js
@@ -0,0 +1,59 @@
+function ScoreList(){
+
+
+    return(
+        <div className="body-component">
+            <h4>SOLERA TEAMS. BOOTCAMP 4</h4>
+            <table className="table-component">
+                <tr className="raw-component">
+                    <th>Team 1</th>
+                    <th>0 puntos</th>
+                </tr>
+                <tr>
+                        <td className="column-one"> Trabajo en equipo 1</td>
+                        <td className="column-two"> 0 puntos</td>
+                </tr>
+                <tr>
+                        <td> Trabajo en equipo 1</td>
+                        <td> 0 puntos</td>
+                </tr>
+                <tr>
+                        <td> Trabajo en equipo 1</td>
+                        <td> 0 puntos</td>
+                </tr>
+                 <tr>
+                        <td> Trabajo en equipo 1</td>
+                        <td> 0 puntos</td>
+                </tr>
+                <tr>
+                        <td> Trabajo en equipo 1</td>
+                        <td> 0 puntos</td>
+                </tr>
+                <tr>
+                        <td> Trabajo en equipo 1</td>
+                        <td> 0 puntos</td>
+                </tr>
+                <tr>
+                        <td> Trabajo en equipo 1</td>
+                        <td> 0 puntos</td>
+                </tr>
+                <tr>
+                        <td> Trabajo en equipo 1</td>
+                        <td> 0 puntos</td>
+                </tr>
+                <tr>
+                        <td> Trabajo en equipo 1</td>
+                        <td> 0 puntos</td>
+                </tr>
+                <tr>
+                        <td> Trabajo en equipo 1</td>
+                        <td> 0 puntos</td>
+                </tr>
+
+            </table>
+        </div>
+
+    );
+}
+
+export default ScoreList;
